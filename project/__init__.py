d1edc27037db1f7e5a7070636e4f04ee99e5c695
Pablonara <138637124+Pablonara@users.noreply.github.com>
2024-12-19 22:24:26 -0800

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_login import login_required, current_user    
+import os
 from .extensions import db
 
 from .models import User 
 def create_app(event=None, context=None):
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:8TKf4LMBNrQ-izo2XAGrR4fKmgwLubRqxJq@postgres-db.cvq642g6w293.us-west-2.rds.amazonaws.com:5432/flask_sql_auth'
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
     print("init db")
     db.init_app(app)
     print("load db")
